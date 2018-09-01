9403fb4f009f61477ccdd9ec8fab81e84b7737f0
Rutwik Choughule <rutwik110@gmail.com>
2018-09-01 18:00:32 +0530

    Updated Snake Game

--- 01-Win32/06-MultiTasking and MultiThreading/KindOfGame/KindOfGame/Game.cpp
+++ 01-Win32/06-MultiTasking and MultiThreading/KindOfGame/KindOfGame/Game.cpp
@@ -1,6 +1,6 @@
 // Headers
 #include <Windows.h>
-#include <tchar.h>
+#include <time.h>
 #include "Snake.h"
 
 
@@ -18,17 +18,23 @@ typedef struct tagCOLORS {
 	HWND hwnd;
 } COLORS, *LPCOLORS;
 
-typedef struct tagPOSITION {
+typedef struct tagFOOD {
 	int iX;
 	int iY;
-} POSITION;
+} FOOD;
+
+typedef struct tagSNAKE {
+	int iDirection;
+	int iLength;
+	int body[SIZE*SIZE][2];
+} SNAKE, *LPSNAKE;
 
 typedef struct tagSTATE {
-	Snake Snake;
+	int iScore;
+	FOOD Food;
+	SNAKE Snake;
 	HWND hwnd;
 } STATE, *LPSTATE;
-
-static POSITION position = { 0, 0 };
 
 
 // WinMain
@@ -47,19 +53,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	wndclass.cbWndExtra = 0;
 	wndclass.lpfnWndProc = WndProc;
 	wndclass.hInstance = hInstance;
-	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
 	wndclass.hbrBackground = (HBRUSH)GetStockObject(DKGRAY_BRUSH);
 	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wndclass.lpszClassName = szClassName;
 	wndclass.lpszMenuName = NULL;
-	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
 
 	// Register class
 	RegisterClassEx(&wndclass);
 
 	// Create Window
 	hwnd = CreateWindow(szClassName,
-		TEXT("Multi Threading"),
+		TEXT("SnAkE!"),
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
@@ -71,7 +77,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 		NULL);
 
 	// Display Window
-	ShowWindow(hwnd, iCmdShow);
+	ShowWindow(hwnd, SW_MAXIMIZE);
 	UpdateWindow(hwnd);
 
 	// Message Loop
@@ -91,23 +97,40 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	static HANDLE hThreadControl = NULL;
 
 	static COLORS colors = { 0, 0, 0, NULL };
-	
+
+	static STATE State;
 
 	RECT rc;
 	HDC hdc;
 	PAINTSTRUCT ps;
 	HBRUSH hBrush;
-	int height, width, size;
+	HBRUSH hbrDarkGreen, hbrLightGreen;
+	TCHAR lpszScore[100];
+	int height, width, size, iResult;
 
 	switch (iMsg)
 	{
 	case WM_CREATE:
 
-		colors.hwnd = hwnd;
+
+		srand((unsigned)time(NULL));
+
+		// Init the STATE
+		State.Food.iX = (SIZE / 3);
+		State.Food.iY = (SIZE / 3);
+		
+		State.Snake.iLength = 10;
+
+		for (int i = 0; i < State.Snake.iLength; i++)
+		{
+			State.Snake.body[i][0] = (SIZE / 2) - i;
+			State.Snake.body[i][1] = (SIZE / 2);
+		}
+
+		State.Snake.iDirection = DIR_RT;
 
 		// Create Threads
-		hThreadMove = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadSnakeMove, (LPVOID)hwnd, 0, NULL);
-
+		hThreadMove = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadSnakeMove, (LPVOID)&State, 0, NULL);
 
 		break;
 
@@ -115,49 +138,63 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 
 		switch (wParam)
 		{
+		case 'P':
+			// Pause Main Snake Thread
+			SuspendThread(hThreadMove);
+
+			iResult = MessageBox(hwnd, TEXT("Press OK to resume"), TEXT("Game Paused"), MB_OK | MB_ICONINFORMATION );
+			if (iResult == IDOK)
+			{
+				// Resume Thread
+				ResumeThread(hThreadMove);
+			}
+			break;
+
 		case 'W':
 		case VK_UP:
-			if (position.iY == 0)
-				position.iY = sizeCanvas;
-			else
-			{
-				position.iY = (position.iY - 1) % sizeCanvas;
-			}
+			if (State.Snake.iDirection != DIR_DW)
+				State.Snake.iDirection = DIR_UP;
 			break;
 
 		case 'A':
 		case VK_LEFT:
-			if (position.iX == 0)
-				position.iX = sizeCanvas;
-			else
-			{
-				position.iX = abs((position.iX - 1) % sizeCanvas);
-			}
+			if (State.Snake.iDirection != DIR_RT)
+				State.Snake.iDirection = DIR_LF;
 			break;
 
 		case 'S':
 		case VK_DOWN:
-			position.iY = (position.iY + 1) % sizeCanvas;
+			if (State.Snake.iDirection != DIR_UP)
+				State.Snake.iDirection = DIR_DW;
 			break;
 
 		case 'D':
 		case VK_RIGHT:
-			position.iX = (position.iX + 1) % sizeCanvas;
+			if (State.Snake.iDirection != DIR_LF)
+				State.Snake.iDirection = DIR_RT;
+			break;
+
+		case VK_ESCAPE:
+
+			// Pause Game
+			SuspendThread(hThreadMove);
+
+			// Confirm Exit
+			iResult = MessageBox(hwnd, TEXT("Do You Want To Quit?"), TEXT("SnAkE!"), MB_YESNO | MB_ICONQUESTION);
+			if (iResult == IDYES) DestroyWindow(hwnd);
+			else ResumeThread(hThreadMove);
+
 			break;
 		}
 
 		GetClientRect(hwnd, &rc);
-		InvalidateRect(hwnd, &rc, FALSE);
+		InvalidateRect(hwnd, &rc, TRUE);
 
-		break;
-
-	case WM_LBUTTONDOWN:
-		MessageBox(hwnd, TEXT("This is 4th Thread!"), TEXT("Message Box"), MB_OK);
 		break;
 
 	case WM_PAINT:
 
-		RECT rcPlayer, rcCanvas;
+		RECT rcUnit, rcCanvas;
 
 		hdc = BeginPaint(hwnd, &ps);
 		GetClientRect(hwnd, &rc);
@@ -166,21 +203,70 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		width = rc.right - rc.left;
 		size = width > height ? height : width;
 
-		rcCanvas.left = rc.left;
+		// Create Canvas
+		rcCanvas.left = rc.left + 80;
 		rcCanvas.top = rc.top;
 		rcCanvas.right = rcCanvas.left + size;
 		rcCanvas.bottom = rcCanvas.top + size;
 
-		rcPlayer.left = rcCanvas.left + (position.iX * (size / sizeCanvas));
-		rcPlayer.top = rcCanvas.top + (position.iY * (size / sizeCanvas));
-		rcPlayer.right = rcPlayer.left + (size / sizeCanvas);
-		rcPlayer.bottom = rcPlayer.top + (size / sizeCanvas);
+		// hbrLightGreen = CreateSolidBrush(RGB(0, 95, 0));
+		// hbrDarkGreen = CreateSolidBrush(RGB(0, 75, 0));
 
+		// Fill Canvas like Grass Pattern
+		/*
+		for (int i = 0; i < SIZE; i++)
+		{
+			for (int j = 0; j < SIZE; j++)
+			{
+				rcUnit.left = rcCanvas.left + (i * (size / SIZE));
+				rcUnit.top = rcCanvas.top + (j * (size / SIZE));
+				rcUnit.right = rcUnit.left + (size / SIZE);
+				rcUnit.bottom = rcUnit.top + (size / SIZE);
+
+				if ((i + j) % 2 == 0)
+				{
+					FillRect(hdc, &rcUnit, hbrLightGreen);
+				}
+				else
+				{
+					FillRect(hdc, &rcUnit, hbrDarkGreen);
+				}
+			}
+		}
+		*/
+
+		// Fill canvas in all black
 		hBrush = CreateSolidBrush(RGB(0, 0, 0));
 		FillRect(hdc, &rcCanvas, hBrush);
-		hBrush = CreateSolidBrush(RGB(255, 255, 255));
-		FillRect(hdc, &rcPlayer, hBrush);
 
+		// Draw Snake
+		for (int i = 0; i < State.Snake.iLength; i++)
+		{
+			rcUnit.left = rcCanvas.left + (State.Snake.body[i][0] * (size / SIZE));
+			rcUnit.top = rcCanvas.top + (State.Snake.body[i][1] * (size / SIZE));
+			rcUnit.right = rcUnit.left + (size / SIZE);
+			rcUnit.bottom = rcUnit.top + (size / SIZE);
+
+			hBrush = CreateSolidBrush(RGB(255 - (i*2), 255 - (i * 2), 255 - (i * 2))); /* White Gradiant Snake*/
+			//hBrush = CreateSolidBrush(RGB(128, 64, 0)); /* Brown Snake*/
+			FillRect(hdc, &rcUnit, hBrush);
+		}
+
+		// Draw Food
+		rcUnit.left = rcCanvas.left + (State.Food.iX * (size / SIZE));
+		rcUnit.top = rcCanvas.top + (State.Food.iY * (size / SIZE));
+		rcUnit.right = rcUnit.left + (size / SIZE);
+		rcUnit.bottom = rcUnit.top + (size / SIZE);
+
+		hBrush = CreateSolidBrush(RGB(255, 0, 0));
+		FillRect(hdc, &rcUnit, hBrush);
+
+		// Draw Score
+		wsprintf(lpszScore, TEXT("Score: %d\nSnake:\n Length: %d\n Head:\n  X: %d\n  Y: %d\nFood:\n X: %d\n Y: %d"), State.iScore, State.Snake.iLength, State.Snake.body[0][0], State.Snake.body[0][1], State.Food.iX, State.Food.iY);
+		SetTextColor(hdc, RGB(0, 0, 0));
+		DrawText(hdc, lpszScore, -1, &rc, DT_LEFT);
+
+		// Finish
 		DeleteObject(hBrush);
 		EndPaint(hwnd, &ps);
 
@@ -189,7 +275,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	case WM_DESTROY:
 
 		// Close Thread handles
-
 		PostQuitMessage(0);
 		break;
 	}
@@ -200,12 +285,65 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 //// Thread Procs
 DWORD ThreadSnakeMove(LPVOID param)
 {
-	while (TRUE) 
+	LPSTATE lpState = (LPSTATE)param;
+
+	while (TRUE)
 	{
-		Sleep(1000);
-		position.iX = position.iX++ % sizeCanvas;
-		position.iY = position.iY++ % sizeCanvas;
-		HWND hwnd = (HWND)param;
+		Sleep(100);
+
+		int iTempX = lpState->Snake.body[lpState->Snake.iLength - 1][0];
+		int iTempY = lpState->Snake.body[lpState->Snake.iLength - 1][1];
+
+		for (int i = lpState->Snake.iLength - 1; i > 0; i--)
+		{
+			lpState->Snake.body[i][0] = lpState->Snake.body[i - 1][0];
+			lpState->Snake.body[i][1] = lpState->Snake.body[i - 1][1];
+		}
+
+		switch (lpState->Snake.iDirection)
+		{
+		case DIR_UP:
+			lpState->Snake.body[0][0] = lpState->Snake.body[1][0];
+
+			// Canvas Edge condition
+			if ((lpState->Snake.body[1][1] - 1) < 0) lpState->Snake.body[0][1] = SIZE;
+			else lpState->Snake.body[0][1] = (lpState->Snake.body[1][1] - 1) % SIZE;
+
+			break;
+
+		case DIR_LF:
+
+			// Canvas Edge condition
+			if ((lpState->Snake.body[1][0] - 1) < 0) lpState->Snake.body[0][0] = SIZE;
+			else lpState->Snake.body[0][0] = (lpState->Snake.body[1][0] - 1) % SIZE;
+
+			lpState->Snake.body[0][1] = lpState->Snake.body[1][1];
+
+			break;
+
+		case DIR_DW:
+			lpState->Snake.body[0][0] = lpState->Snake.body[1][0];
+			lpState->Snake.body[0][1] = (lpState->Snake.body[1][1] + 1) % (SIZE + 1);
+			break;
+
+		case DIR_RT:
+			lpState->Snake.body[0][0] = (lpState->Snake.body[1][0] + 1) % (SIZE + 1);
+			lpState->Snake.body[0][1] = lpState->Snake.body[1][1];
+			break;
+		}
+
+		// If Snake head is on the food
+		if (lpState->Food.iX == lpState->Snake.body[0][0] && lpState->Food.iY == lpState->Snake.body[0][1])
+		{
+			lpState->Snake.body[lpState->Snake.iLength][0] = iTempX;
+			lpState->Snake.body[lpState->Snake.iLength][1] = iTempY;
+			lpState->Snake.iLength++;
+			lpState->iScore += 10;
+			lpState->Food.iX = (double)rand() / (RAND_MAX + 1) * (SIZE);
+			lpState->Food.iY = (double)rand() / (RAND_MAX + 1) * (SIZE);
+		}
+
+		HWND hwnd = lpState->hwnd;
 		RECT rc;
 		HDC hdc = GetDC(hwnd);
 		GetClientRect(hwnd, &rc);
@@ -215,53 +353,3 @@ DWORD ThreadSnakeMove(LPVOID param)
 	return(0);
 }
 
-//DWORD ThreadProcTwo(LPVOID param)
-//{
-//
-//	HWND hwnd = (HWND)param;
-//	TCHAR str[255];
-//
-//	HDC hdc = GetDC(hwnd);
-//	SetTextColor(hdc, RGB(0, 255, 0));
-//	SetBkColor(hdc, RGB(0, 0, 0));
-//	for (int i = 32676; i >= 0; i--)
-//	{
-//		wsprintf(str, TEXT("Decrementing: %d"), i);
-//		TextOut(hdc, 5, 25, str, _tcsclen(str));
-//	}
-//
-//	ReleaseDC(hwnd, hdc);
-//	return(0);
-//}
-//
-//DWORD ThreadProcThree(LPVOID param)
-//{
-//	RECT rc;
-//	LPCOLORS colors = (LPCOLORS)param;
-//	for (int i = 0; i <= 255; i++) {
-//		colors->iR = colors->iG = colors->iB = i;
-//		GetClientRect(colors->hwnd, &rc);
-//		InvalidateRect(colors->hwnd, &rc, TRUE);
-//		Sleep(30);
-//	}
-//	return(0);
-//}
-//
-//DWORD ThreadProcFour(LPVOID param)
-//{
-//	RECT rc;
-//	LPCOLORS colors = (LPCOLORS)param;
-//	for (colors->iR = 0; colors->iR <= 255; colors->iR++) {
-//		for (colors->iG = 0; colors->iG <= 255; colors->iG++) {
-//			colors->iR++;
-//			for (colors->iB = 0; colors->iB <= 255; colors->iB++) {
-//				colors->iR++;
-//				colors->iB++;
-//				GetClientRect(colors->hwnd, &rc);
-//				InvalidateRect(colors->hwnd, &rc, TRUE);
-//				Sleep(10);
-//			}
-//		}
-//	}
-//	return(0);
-//}
--- 01-Win32/06-MultiTasking and MultiThreading/KindOfGame/KindOfGame/Snake.h
+++ 01-Win32/06-MultiTasking and MultiThreading/KindOfGame/KindOfGame/Snake.h
@@ -1,86 +1,10 @@
+// Resources
+#define MYICON 101
+
 // Directions
 #define DIR_UP 0
 #define DIR_DW 1
 #define DIR_LF 2
 #define DIR_RT 3
 
-// Global Variables
-int sizeCanvas = 50;
-
-class SnakeBody {
-private:
-	int iX;
-	int iY;
-
-public:
-	SnakeBody *lpsbNext;
-	SnakeBody *lpsbPrev;
-
-	inline int GetX() {
-		return iX;
-	};
-	inline int GetY() {
-		return iY;
-	};
-	inline void SetX(int _iX) {
-		iX = _iX;
-	};
-	inline void SetY(int _iY) {
-		iY = _iY;
-	};
-};
-
-class Snake {
-
-private:
-	int iDirection;
-	int iLength;
-	SnakeBody *lpsbSnake;
-
-public:
-	void SetDirection(int iDir);
-	int GetDirection();
-	void GrowSnake();
-	void MoveSnake();
-};
-
-int Snake::GetDirection()
-{
-	return iDirection;
-}
-
-void Snake::SetDirection(int iNewDirection)
-{
-	iDirection = iNewDirection;
-}
-
-void Snake::GrowSnake()
-{
-	SnakeBody snakeHead = *lpsbSnake;
-
-	switch (iDirection)
-	{
-	case DIR_UP:
-		break;
-
-	case DIR_DW:
-		break;
-
-	case DIR_LF:
-		break;
-
-	case DIR_RT:
-		break;
-	}
-/*
-	sbBody.SetX(lpsbSnake->GetX());
-	sbBody.SetY(lpsbSnake->GetY());*/
-	lpsbSnake->GetY();
-
-	//while(lpsbSnake)
-}
-
-void Snake::MoveSnake()
-{
-	//lpsbSnake.
-}
+#define SIZE 50
